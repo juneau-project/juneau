2cbb2829a60daa60556b71b2afab07af78095af2
Nacho <nacho@navarromunoz.com>
2020-07-08 11:01:55 +0200

    Clean up the jupyter module.

--- juneau/jupyter/jupyter.py
+++ juneau/jupyter/jupyter.py
@@ -1,3 +1,22 @@
+# Copyright 2020 Juneau
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module is in charge of defining functions that will
+interact with the Jupyter kernel.
+"""
+
 import logging
 import os
 import subprocess
@@ -16,14 +35,12 @@ def request_var(kid, var):
     """
     Requests the contents of a dataframe or matrix by executing some code.
     """
-    code = (
-        f"""
+    code = f"""
         import pandas as pd
         import numpy as np
         if isinstance({var}, pd.DataFrame) or isinstance({var}, np.ndarray) or isinstance({var}, list):
             print({var}.to_json(orient='split', index=False))
         """
-    )
     return exec_code(kid, code)
 
 
@@ -47,12 +64,16 @@ def exec_code(kid, code):
 
         while km.is_alive():
             msg = km.get_iopub_msg(timeout=10)
-            if 'content' in msg and 'name' in msg['content'] and msg['content']['name'] == 'stdout':
-                output = msg['content']['text']
+            if (
+                "content" in msg
+                and "name" in msg["content"]
+                and msg["content"]["name"] == "stdout"
+            ):
+                output = msg["content"]["text"]
                 break
 
         km.stop_channels()
-        if reply['content']['status'] != 'ok':
+        if reply["content"]["status"] != "ok":
             logging.error(f"Status is {reply['content']['status']}")
             logging.error(output)
             error = output
@@ -71,11 +92,11 @@ def exec_connection_to_psql(kernel_id):
         tuple - the output of the code, and the error if any.
     """
     with jupyter_lock:
-        psql_connection = os.path.join(os.path.join(BASE_DIR, 'db'), 'connect_psql.py')
+        psql_connection = os.path.join(os.path.join(BASE_DIR, "db"), "connect_psql.py")
         msg_id = subprocess.Popen(
-            ['python3', psql_connection, kernel_id],
+            ["python3", psql_connection, kernel_id],
             stderr=subprocess.PIPE,
-            stdout=subprocess.PIPE
+            stdout=subprocess.PIPE,
         )
         output, error = msg_id.communicate()
 
