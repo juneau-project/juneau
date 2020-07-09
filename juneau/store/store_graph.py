93a011f8cf959a93b46a3fa994834a6a384864c1
Nacho <nacho@navarromunoz.com>
2020-07-09 09:53:42 +0200

    Clean up store_graph.py

--- juneau/store/store_graph.py
+++ juneau/store/store_graph.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+TODO: Explain what this module does.
+"""
+
 import base64
 import logging
 
@@ -25,7 +29,10 @@ class ProvenanceStorage:
         self.postgres_eng = postgres_eng
         self.code_dict = {}
 
-    def __initialize_code_dict(self):
+    def _initialize_code_dict(self):
+        """
+        TODO: Explain what this function does.
+        """
 
         query1 = "DROP SCHEMA IF EXISTS nb_provenance CASCADE;"
         query2 = "CREATE SCHEMA nb_provenance;"
@@ -46,87 +53,75 @@ class ProvenanceStorage:
                 logging.error(
                     f"Store Provenance: creation of provenance schema failed with error {e}"
                 )
-            finally:
-                conn.close()
 
-    def __fetch_code_dict(self):
-        conn = self.postgres_eng.connect()
-        try:
-            code_table = pd.read_sql_table("code_dict", conn, schema="nb_provenance")
-            for index, row in code_table.iterrows():
-                self.code_dict[row["code"]] = int(row["cell_id"])
-            conn.close()
-            return True
-        except Exception as e:
-            logging.error(f"Store Provenance: reading code table failed with error {e}")
-            conn.close()
-            return False
+    def _fetch_code_dict(self):
+        """
+        TODO: Explain what this function does.
+        """
 
-    def store_code_dict(self):
+        with self.postgres_eng.connect() as conn:
+            try:
+                code_table = pd.read_sql_table(
+                    "code_dict", conn, schema="nb_provenance"
+                )
+                for index, row in code_table.iterrows():
+                    self.code_dict[row["code"]] = int(row["cell_id"])
+            except Exception as e:
+                logging.error(
+                    f"Store Provenance: reading code table failed with error {e}"
+                )
+
+    def _store_code_dict(self):
+        """
+        TODO: Explain what this function does.
+        """
+
         dict_store = {"code": [], "cell_id": []}
         for i in self.code_dict.keys():
             dict_store["code"].append(i)
             dict_store["cell_id"].append(self.code_dict[i])
         dict_store_code = pd.DataFrame.from_dict(dict_store)
-        conn = self.postgres_eng.connect()
-        dict_store_code.to_sql(
-            "code_dict", conn, schema="nb_provenance", if_exists="replace"
-        )
-        conn.close()
-        return True
+        with self.postgres_eng.connect() as conn:
+            dict_store_code.to_sql(
+                "code_dict", conn, schema="nb_provenance", if_exists="replace"
+            )
 
     def add_cell(self, code, prev_node, var, cell_id, nb_name):
         """
         Stores the Jupyter cell in base64 encoded form, and adds a link to the previously
-        stored node (for cell provenance tracking)
-
-        :param code:
-        :param prev_node:
-        :param var:
-        :param cell_id:
-        :param nb_name:
-        :return:
+        stored node (for cell provenance tracking).
         """
-        self.__fetch_code_dict()
-
-        bcode = str(base64.b64encode(bytes(code, "utf-8")))
+        self._fetch_code_dict()
+        bcode = base64.b64encode(bytes(code, "utf-8"))
+        nbcode = bcode.decode("utf-8")
         matcher = NodeMatcher(self.graph_db)
 
-        # logging.info(bcode)
-        nbcode = bcode[2:-1]
-        # logging.info(list(self.code_dict.items())[:10])
-
-        if bcode in self.code_dict:
-            current_cell = matcher.match("Cell", source_code=bcode).first()
-        elif nbcode in self.code_dict:
+        if bcode in self.code_dict or nbcode in self.code_dict:
             current_cell = matcher.match("Cell", source_code=bcode).first()
         else:
             if len(list(self.code_dict.values())) != 0:
                 max_id = max(list(self.code_dict.values()))
             else:
                 max_id = 0
-            current_cell = Node(
-                "Cell", name="cell_" + str(max_id + 1), source_code=bcode
-            )
+            current_cell = Node("Cell", name=f"cell_{max_id + 1}", source_code=bcode)
             self.graph_db.create(current_cell)
             self.graph_db.push(current_cell)
 
             if prev_node is not None:
-                cell_edge = Relationship(prev_node, "Successor", current_cell)
-                self.graph_db.create(cell_edge)
-                self.graph_db.push(cell_edge)
-                cell_edge2 = Relationship(current_cell, "Parent", prev_node)
-                self.graph_db.create(cell_edge2)
-                self.graph_db.push(cell_edge2)
+                successor = Relationship(prev_node, "Successor", current_cell)
+                self.graph_db.create(successor)
+                self.graph_db.push(successor)
+                parent = Relationship(current_cell, "Parent", prev_node)
+                self.graph_db.create(parent)
+                self.graph_db.push(parent)
 
             self.code_dict[bcode] = max_id + 1
             try:
-                self.store_code_dict()
-            except:
-                logging.info("update code for neo4j failed!")
+                self._store_code_dict()
+            except Exception as e:
+                logging.info(f"Code update for Neo4j failed with error {e}")
 
-        var_name = str(cell_id) + "_" + var + "_" + nb_name
-
+        var_name = f"{cell_id}_{var}_{nb_name}"
         current_var = matcher.match("Var", name=var_name).first()
 
         if current_var is None:
@@ -135,11 +130,11 @@ class ProvenanceStorage:
             self.graph_db.create(current_var)
             self.graph_db.push(current_var)
 
-            edge = Relationship(current_cell, "Contains", current_var)
-            edge2 = Relationship(current_var, "Containedby", current_cell)
-            self.graph_db.create(edge)
-            self.graph_db.push(edge)
-            self.graph_db.create(edge2)
-            self.graph_db.push(edge2)
+            contains_edge = Relationship(current_cell, "Contains", current_var)
+            self.graph_db.create(contains_edge)
+            self.graph_db.push(contains_edge)
+            contained_by_edge = Relationship(current_var, "Containedby", current_cell)
+            self.graph_db.create(contained_by_edge)
+            self.graph_db.push(contained_by_edge)
 
         return current_cell
